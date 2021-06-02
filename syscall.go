0a4e931188422232d6eb9218dc4543ec6aee3034
Gijs Peskens <gijs@peskens.net>
2021-06-02 23:44:14 +0200

    Move param functions to own file, for better overview
    Also improve paramToIOVec parsing & documentation.

--- syscall.go
+++ syscall.go
@@ -2,14 +2,11 @@ package gojail
 
 import (
 	"errors"
-	"fmt"
-	"golang.org/x/sys/unix"
-	"net"
-	"reflect"
 	"strconv"
-	"strings"
 	"syscall"
 	"unsafe"
+
+	"golang.org/x/sys/unix"
 )
 
 //JailID is used to identify jails
@@ -96,27 +93,6 @@ func JailGetID(name string) (JailID, error) {
 	return JailGet(iovecs, 0)
 }
 
-//JailParseParametersToIovec parses a map[string]interface{} parameter set to []syscall.Iovec
-//for use in Jail syscalls requiring []syscall.Iovec
-//Byte slices & pointers are considered out variables and will be filled with JailGet.
-//for setting handing over variables use strings or ints instead.
-//gojail uses errmsg, and will error out if it's passed as a key
-//No validation is done w.r.t. the type required by the jail parameter
-func JailParseParametersToIovec(parameters map[string]interface{}) ([]syscall.Iovec, error) {
-	iovecs := make([]syscall.Iovec, 0)
-	for key, value := range parameters {
-		if key == "errmsg" {
-			return nil, errors.New("Usage of errmsg is reserved by gojail")
-		}
-		parIovec, err := paramToIOVec(key, value)
-		if err != nil {
-			return nil, err
-		}
-		iovecs = append(iovecs, parIovec...)
-	}
-	return iovecs, nil
-}
-
 func jailIOVSyscall(callnum uintptr, iovecs []syscall.Iovec, flags int) (JailID, error) {
 	errbuf, erriov := makeErrorIov()
 
@@ -138,107 +114,6 @@ func jailJidSyscall(callnum uintptr, jid JailID) error {
 		return errno
 	}
 	return nil
-}
-
-func paramToIOVec(key string, value interface{}) ([]syscall.Iovec, error) {
-	var val *byte
-
-	name, err := syscall.ByteSliceFromString(key)
-	if err != nil {
-		return nil, err
-	}
-	valsize := int(4)
-	switch v := value.(type) {
-	case int32:
-		val = (*byte)(unsafe.Pointer(&v))
-	case uint32:
-		val = (*byte)(unsafe.Pointer(&v))
-	case JailID:
-		val = (*byte)(unsafe.Pointer(&v))
-	case *int32:
-		val = (*byte)(unsafe.Pointer(v))
-	case *uint32:
-		val = (*byte)(unsafe.Pointer(v))
-	case *JailID:
-		val = (*byte)(unsafe.Pointer(&v))
-	case string:
-		//Special case: some variables either "disabled", "new" or "inherit"
-		//in normal config, but actually map to 0, 1 ,2
-		switch v {
-		case "disabled":
-			tmp := int32(0)
-			val = (*byte)(unsafe.Pointer(&tmp))
-		case "new":
-			tmp := int32(1)
-			val = (*byte)(unsafe.Pointer(&tmp))
-		case "inherit":
-			tmp := int32(2)
-			val = (*byte)(unsafe.Pointer(&tmp))
-		default:
-			val, err = syscall.BytePtrFromString(v)
-			if err != nil {
-				return nil, err
-			}
-			valsize = len(v) + 1
-		}
-	case []byte:
-		val = &v[0]
-		valsize = len(v)
-	case []net.IP:
-		state := 0 //0 -> not initialized, 1 -> ipv4, 2 -> ipv6
-		tmp := make([]byte, 0)
-		for _, ip := range v {
-			ipv4 := ip.To4()
-			if ipv4 == nil {
-				if state == 1 {
-					return nil, errors.New("Mixing ipv4 & ipv6 not allowed")
-				}
-				state = 2
-				tmp = append(tmp, []byte(ip)...)
-			} else {
-				if state == 2 {
-					return nil, errors.New("Mixing ipv4 & ipv6 not allowed")
-				}
-				state = 1
-				tmp = append(tmp, ipv4...)
-			}
-			val = &tmp[0]
-			valsize = len(tmp)
-		}
-
-	case bool:
-		if strings.Contains(key, ".") {
-			//nothing for now, workaround booleans like ip4.saddrsel/ip4.nosaddrsel
-		} else if !v && !strings.HasPrefix(key, "no") {
-			tmp, err := syscall.ByteSliceFromString("no" + key)
-			if err != nil {
-				return nil, err
-			}
-			name = tmp
-		} else if v && strings.HasPrefix(key, "no") {
-			tmp, err := syscall.ByteSliceFromString(strings.TrimPrefix(key, "no"))
-			if err != nil {
-				return nil, err
-			}
-			name = tmp
-		}
-		val = nil
-		valsize = 0
-	default:
-		return nil, fmt.Errorf("paramToIOVec: type: %s not implemented", reflect.TypeOf(v))
-	}
-	return makeJailIovec(name, val, valsize), nil
-}
-
-func makeJailIovec(name []byte, value *byte, valuesize int) []syscall.Iovec {
-	iovecs := make([]syscall.Iovec, 2)
-
-	iovecs[0].Base = &name[0]
-	iovecs[0].SetLen(len(name))
-
-	iovecs[1].Base = value
-	iovecs[1].SetLen(valuesize)
-	return iovecs
 }
 
 func makeErrorIov() ([]byte, []syscall.Iovec) {
